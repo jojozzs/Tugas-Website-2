b858b96cc89cc59af7e43731550b7503bb79a24a
Jo <Jonathanchdra@gmail.com>
2024-09-25 19:21:21 +0800

    seles

--- changingtext.js
+++ changingtext.js
@@ -1,19 +1,18 @@
 import $ from "jquery";
 
-const changingtext = (changetext) => {
+const changingtext = (element) => {
     console.log("tes");
     console.log(element);
     element.text('Click Here to Change Text');
     
     const isiElement = $('#isi'); 
     console.log(isiElement); 
-    
-    isiElement.text("Hello World 2");
-    
     element.click(() => {
-        isiElement.text("Hello World 3");
+        isiElement.text("Hello World 2");
+        element.click(() => {
+         isiElement.text("Hello World 3");
     });
+});
 };
 
-console.log("wlaw");
 export default changingtext;
--- main.js
+++ main.js
@@ -7,12 +7,6 @@ import changingtext from "./changingtext.js";
 
 $('#app').html( `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
     <h1 id="isi">Hello Vite!</h1>
     <div class="card">
       <button id="changetext" type="button"></button>
@@ -25,4 +19,4 @@ $('#app').html( `
 `
 )
 
-changingtext($('#changetext)'))
+changingtext($('#changetext'))
